c074d5eb8475ab89adf3e28073d9a4f2a5a32d4f
Vinod Jatav <jatavvinod1997@gmail.com>
2021-08-16 15:58:09 +0530

    Blog functionalities

--- blog/models.py
+++ blog/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.db.models.fields import DateTimeField, CharField, TextField
+from django.shortcuts import render
 
 class Blog(models.Model):
     title = models.CharField(max_length=100)
@@ -7,3 +8,12 @@ class Blog(models.Model):
     body = models.TextField(max_length=500)
     image = models.ImageField(upload_to='images/')
 
+    def __str__(self):
+        return self.title
+
+        return super().__str__()
+    def summary(self):
+        return self.body[:100]
+    
+    def pub_date_pretty(self):
+        return self.pub_date.strftime('%d %B, %Y')
--- portfolio/urls.py
+++ portfolio/urls.py
@@ -2,8 +2,10 @@ from django.contrib import admin
 from django.urls import path
 from django.conf import settings
 from django.conf.urls.static import static
+from django.urls.conf import include
 import jobs.views
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', jobs.views.home, name='home'),
+    path('blog/', include('blog.urls')),
 ] + static(settings.MEDIA_URL,document_root = settings.MEDIA_ROOT)
